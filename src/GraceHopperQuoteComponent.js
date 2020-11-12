97fbaadd1988d1b9b3791347ac73b86ef1a38b71
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-11-12 21:33:46 +0000

    Automatically backed up by Learn

--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -16,19 +16,4 @@ export default class GraceHopperQuoteComponent extends Component {
       </div>
     );
   }
-=======
-	render() {
-		return (
-			<div className="bar" id="hopper">
-				<i>
-					<p>
-						&quot;The most dangerous phrase in the language is, ‘We’ve always done it this way.’&quot;
-					</p>
-				</i>
-				<br />
-				&#9;-Admiral Grace Hopper
-			</div>
-		);
-	}
->>>>>>> 7cce0229f6600763d35c290ada1eaf806d0e44a6
 }
