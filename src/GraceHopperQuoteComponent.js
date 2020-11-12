0a6cad960fa6f2525630d357ff17d5f54ceade12
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-11-12 21:34:08 +0000

    Automatically backed up by Learn

--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 export default class GraceHopperQuoteComponent extends Component {
-<<<<<<< HEAD
   render() {
     return (
       <div className="bar" id="hopper">
